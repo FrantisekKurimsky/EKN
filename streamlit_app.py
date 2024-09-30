21ed3692e05949f811bdfaa010e97d3ced97813f
FrantisekKurimsky <feri.kurimsky@gmail.com>
2024-09-30 09:58:26 +0200

    init test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,33 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
+
+st.sidebar.title("Navigation")
+pages = st.sidebar.radio("Go to", ["Domov", "1. Cvičenie"])
+
+def home_page():
+    st.title("Ekonomika v elektroenergetike - cvičenia")
+
+# Function to display the Math Problem page
+def first():
+    st.title("Math Problem")
+    st.write("Here is a math problem formatted using LaTeX:")
+    
+    # Display a simple LaTeX math problem
+    st.latex(r'''
+    \text{Solve for } x \text{ in the equation:}
+    ax^2 + bx + c = 0
+    ''')
+    
+    st.write("This is the quadratic equation. The solution is given by:")
+    
+    # Display the quadratic formula in LaTeX
+    st.latex(r'''
+    x = \frac{-b \pm \sqrt{b^2 - 4ac}}{2a}
+    ''')
+
+# Navigation logic
+if pages == "Home":
+    home_page()
+elif pages == "Math Problem":
+    first()
