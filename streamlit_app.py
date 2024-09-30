f2cc4d03c9a9d7b5c9b8966078b9d48ba66e5012
FrantisekKurimsky <feri.kurimsky@gmail.com>
2024-09-30 10:37:23 +0200

    remove subheader, change dropdown menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,14 +33,13 @@ def first():
     slide_index = st.selectbox(
         "Select a math problem:",
         list(range(len(math_problems))),
-        format_func=lambda x: f"Problem {x + 1}"
+        format_func=lambda x: f"Príklad: 1.0{slide_index+1}"
     )
     
     
     # Display the current math problem and solution
     problem = math_problems[slide_index]
-    
-    st.subheader(f"Príklad: 1.0{slide_index+1}")
+
     st.latex(problem["question"])
     
     with st.expander("Riešenie"):
