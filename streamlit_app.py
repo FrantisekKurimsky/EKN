2a15f8367b784fedd189e6ba7c1d5b1a4afbe1fc
FrantisekKurimsky <feri.kurimsky@gmail.com>
2024-09-30 11:14:46 +0200

    add priklad 6

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,27 +4,27 @@ import pandas as pd
 
 math_problems_1 = [
     {
-        "question": r"\text{Banka poskytuje} 11 \% \text{ročný úrok na uložených vkladoch. Banka pripisuje úroky v poslednom dni každého štvrťroka. Peniaze uložené na vklad sa úročia na bežný mesiac, ak sú vložené do 8. dňa v bežnom mesiaci. Občan si otvoril účet 8. januára a vložil 5000 €. Aký veľký úrok získa do 30. júna?}",
+        "question": "Banka poskytuje} 11 % ročný úrok na uložených vkladoch. Banka pripisuje úroky v poslednom dni každého štvrťroka. Peniaze uložené na vklad sa úročia na bežný mesiac, ak sú vložené do 8. dňa v bežnom mesiaci. Občan si otvoril účet 8. januára a vložil 5000 €. Aký veľký úrok získa do 30. júna?",
         "solution": r"x = \frac{-b \pm \sqrt{b^2 - 4ac}}{2a}",
         "table": None
     },
     {
-        "question": r"\text{Banka poskytuje na vkladoch 8 % ročný úrok. Karol potrebuje za 9 mesiacov vrátiť dlžobu 5000 €. Koľko musí  teraz vložiť do banky, aby mal za 9 mesiacov k dispozícii práve túto sumu?}",
+        "question": "Banka poskytuje na vkladoch 8 % ročný úrok. Karol potrebuje za 9 mesiacov vrátiť dlžobu 5000 €. Koľko musí  teraz vložiť do banky, aby mal za 9 mesiacov k dispozícii práve túto sumu?",
         "solution": r"f'(x) = 9x^2 + 4x - 5",
         "table": None
     },
     {
-        "question": r"\text{Helena investovala 5000 € na zamestnanecký účet, ktorý prináša 8 % ročný úrok. Ako dlho má ponechať túto sumu na účte, aby získala 300 €?}",
+        "question": "Helena investovala 5000 € na zamestnanecký účet, ktorý prináša 8 % ročný úrok. Ako dlho má ponechať túto sumu na účte, aby získala 300 €?",
         "solution": r"\frac{1}{3}",
         "table": None
     },
     {
-        "question": r"\text{Inštalovaný výkon modelovej elektrizačnej sústavy je koncom roka 2009  Pi = 234 GW. Aký veľký inštalovaný výkon bude mať táto sústava roku 2015, ak predpokladáme každoročný vzrast inštalovaného výkonu o 6 %?}",
+        "question": "Inštalovaný výkon modelovej elektrizačnej sústavy je koncom roka 2009  Pi = 234 GW. Aký veľký inštalovaný výkon bude mať táto sústava roku 2015, ak predpokladáme každoročný vzrast inštalovaného výkonu o 6 %?",
         "solution": r"\frac{1}{3}",
         "table": None
     },
     {
-        "question": r"\text{Na účely budúcej investičnej výstavby začiatkom roku 2010 ukladá podnik do banky podľa dlhodobého finančného plánu na konci uvedených rokov čiastky podľa nasledujúcej tabuľky. Akú  čiastku bude mať podnik k dispozícii pri začatí výstavby, ak úroková miera  p = 6 % ?}",
+        "question": "Na účely budúcej investičnej výstavby začiatkom roku 2010 ukladá podnik do banky podľa dlhodobého finančného plánu na konci uvedených rokov čiastky podľa nasledujúcej tabuľky. Akú  čiastku bude mať podnik k dispozícii pri začatí výstavby, ak úroková miera  p = 6 % ?",
         "table": pd.DataFrame({
             "Rok": [int(x) for x in [2000, 2001, 2002, 2004, 2006, 2008]],
             "K (mil. Eur)": [3.8, 4.1, 3.6, 3.9, 4.0, 3.7]
@@ -32,9 +32,13 @@ math_problems_1 = [
         "solution": r"\frac{1}{3}"
     },
     {
-        "question": r"\text{Helena investovala 5000 € na zamestnanecký účet, ktorý prináša 8 % ročný úrok. Ako dlho má ponechať túto sumu na účte, aby získala 300 €?}",
+        "question": "Rozhodnite, o aký investičný úver má dnes požiadať podnik banku, aby pri úrokovej miere    p =  6 % mohol tento úver práve splatiť po 9 rokoch predpokladanými voľnými finančnými zdrojmi (odpismi a časťou zisku), ktoré sa budú podľa dlhodobého plánu podniku tvoriť tak, ako je vidieť z nasledujúcej tabuľky:",
         "solution": r"\frac{1}{3}",
-        "table": None
+        "table": pd.DataFrame({
+            "Rok": [int(x) for x in [1, 2, 4, 6, 8, 9]],
+            "K (mil. Eur)": [13, 15, 16, 17, 18, 17]
+        }).T,
+
     },
 ]
 
@@ -62,7 +66,7 @@ def first():
     # Display the current math problem and solution
     problem = math_problems_1[slide_index]
     
-    st.latex(problem["question"])
+    st.write(problem["question"])
     if problem['table'] is not None:
         st.write(problem['table'])
     
